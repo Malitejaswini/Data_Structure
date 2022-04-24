bbe2bb291fa8b62c6c00071ca043f983f4777f5a
Tejaswini Mali <99062996+Malitejaswini@users.noreply.github.com>
2022-04-24 22:00:25 +0530

    Update traverseinarray.cpp

--- traverseinarray.cpp
+++ traverseinarray.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
-using namespace std;
-
-int main()
+#include<iostream.h>
+ #include<conio.h>
+ void main()
  {
-   int arr[7] = {25, 63, 74, 69, 81, 65, 68};
-   for (int element: arr)
-   {
-      cout << element << "  ";
-   }
+ int a[50],i,n,t;
+ cout<<"Enter size of array:";
+ cin>>n;
+ cout<<"Enter array elements: "<<endl;
+ for(i=0;i<n;i++)
+ {
+ cin>>a[i];
  }
+ cout<<"You entered array elements are: "<<endl;
+ for(i=0;i<n;i++)
+ {
+ cout<<a[i]<<endl;
+ }
+
+ cout<<"Enter index no: ";
+ cin>>t;
+ cout<<"Element at index "<<t<<" is: "<<a[t];
+ getch();
+ clrscr();
+ }
+
+
+
+
+ 
+ 
+   
+   
+     
+   
+
